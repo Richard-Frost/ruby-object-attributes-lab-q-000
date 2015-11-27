54508312a191b6f63c05afa63eae3f08544688a0
Richard-Frost <frost.rchrd@gmail.com>
2015-11-27 18:15:24 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 class Person
   def name
     @name
-end
+  end
   def name=(new_name)
     @name = new_name
   end
-  def job 
-    @job 
+  def job
+    @job
   end
   def job=(new_job)
     @job = new_job
